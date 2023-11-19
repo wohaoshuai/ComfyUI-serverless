b50a0cbcbd7dce58d619dbd2dbd8312ab95d989a
Tony Kim <wukelong123@gmail.com>
2023-11-19 17:56:59 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import json
 api = ComfyConnector()
 # running = api.is_api_running()
 # print('running', running)
-prompt = json.load(open('test.json'))
+prompt = json.load(open('clean-small.json'))
+print(prompt)
 images = api.generate_images(prompt) 
 print('images')
 # for i, image in enumerate(images):
