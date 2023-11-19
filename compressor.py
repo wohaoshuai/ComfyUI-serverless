8e35ee43ee9aa62428ad4233f5cb392a09ee1c0d
Tony Kim <wukelong123@gmail.com>
2023-11-19 18:26:34 +0800

    images

---
+++ compressor.py
@@ -0,0 +1,53 @@
+from PIL import Image
+import zlib
+import base64
+import io
+
+def encode_images(images, quality=80):
+    encoded_images = []
+    for i, image in enumerate(images):
+        # Resize the image to 256x256
+        resized_image = image.resize((256, 256))
+        
+        # Compress the image with lossy quality
+        compressed_image = io.BytesIO()
+        resized_image.save(compressed_image, format='JPEG', quality=quality)
+        compressed_image = compressed_image.getvalue()
+        
+        # Compress the image bytes using zlib
+        compressed_bytes = zlib.compress(compressed_image)
+        
+        # Encode the compressed bytes as base64
+        encoded_bytes = base64.b64encode(compressed_bytes)
+        
+        # Append the encoded image to the list
+        encoded_images.append(encoded_bytes)
+    
+    return encoded_images
+
+def save_encoded_images_to_file(encoded_images, filename):
+    with open(filename, "w") as f:
+        for encoded_image in encoded_images:
+            f.write(encoded_image.decode() + "\n")
+
+def read_encoded_images_from_file(filename):
+    with open(filename, "r") as f:
+        encoded_images = [line.strip() for line in f]
+    return encoded_images
+
+def decode_images(encoded_images):
+    decoded_images = []
+    for i, encoded_image in enumerate(encoded_images):
+        # Decode the base64-encoded image
+        compressed_bytes = base64.b64decode(encoded_image)
+        
+        # Decompress the image bytes using zlib
+        decompressed_bytes = zlib.decompress(compressed_bytes)
+        
+        # Create a PIL Image from the decompressed bytes
+        image = Image.open(io.BytesIO(decompressed_bytes))
+        
+        # Append the decoded image to the list
+        decoded_images.append(image)
+    
+    return decoded_images
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from comfy_serverless import ComfyConnector
 import json
+from compressor import save_encoded_images_to_file, encode_images, decode_images, read_encoded_images_from_file
 
 api = ComfyConnector()
 # running = api.is_api_running()
@@ -9,6 +10,24 @@ prompt = json.load(open('workflow_api.json'))
 print(prompt)
 images = api.generate_images(prompt) 
 print('images', images)
+# if images and len(images) > 0:
+#     for i, image in enumerate(images):
+#         image.save(f"outputs/test{i}.png")
+
+# Assuming you have a list of images called "images"
 if images and len(images) > 0:
-    for i, image in enumerate(images):
+    # Encode the images into a single file
+    encoded_images = encode_images(images)
+    
+    # Save the encoded images to a file
+    save_encoded_images_to_file(encoded_images, "encoded_images.txt")
+    
+    # Read the encoded images from the file
+    encoded_images = read_encoded_images_from_file("encoded_images.txt")
+    
+    # Decode the images back into a list of PIL Images
+    decoded_images = decode_images(encoded_images)
+    
+    # Save the decoded images to individual files
+    for i, image in enumerate(decoded_images):
         image.save(f"outputs/test{i}.png")
