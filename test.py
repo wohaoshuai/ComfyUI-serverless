65925a3eaa553f42b61dac99704ed2e89b0d5d22
Tony Kim <wukelong123@gmail.com>
2023-11-19 17:36:48 +0800

    save image

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from comfy_serverless import ComfyConnector
+import json
 
 api = ComfyConnector()
-running = api.is_api_running()
-print('running', running)
+# running = api.is_api_running()
+# print('running', running)
+prompt = json.load(open('test.json'))
+images = api.generate_images(prompt) 
+for i, image in enumerate(images):
+    image.save(f"test{i}.png")
