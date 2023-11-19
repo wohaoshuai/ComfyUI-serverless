b59c7efa834db35cf5884a0a3e97b0a094af877d
Tony Kim <wukelong123@gmail.com>
2023-11-19 17:37:40 +0800

    Update test.py

--- test.py
+++ test.py
@@ -6,5 +6,6 @@ api = ComfyConnector()
 # print('running', running)
 prompt = json.load(open('test.json'))
 images = api.generate_images(prompt) 
-for i, image in enumerate(images):
-    image.save(f"test{i}.png")
+print('images')
+# for i, image in enumerate(images):
+#     image.save(f"test{i}.png")
