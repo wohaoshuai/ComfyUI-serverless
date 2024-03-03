2d0eb846b99adb194751664451d44f90d323c9ef
Tony Kim <wukelong123@gmail.com>
2024-03-02 23:38:50 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-nohup python ~/ComfyUI/main.py --listen > output_comfy.log 2>&1 &
-cd ~/ComfyUI-serverless
-nohup python test.py --listen > output_serverless.log 2>&1 &
+#!/bin/bash
+nohup python /home/contact/ComfyUI/main.py --listen > output_comfy.log 2>&1 &
+nohup python /home/contact/ComfyUI-serverless/test.py --listen > output_serverless.log 2>&1 &
