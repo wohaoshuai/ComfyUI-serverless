d9485899952774d98868ced9c2ebdd242a399bdd
Tony Kim <wukelong123@gmail.com>
2024-03-07 13:34:03 -0800

    Update test.py

--- test.py
+++ test.py
@@ -19,37 +19,10 @@ import torch
 import sys 
 from diffusers.utils import load_image, export_to_video, export_to_gif
 
-from sfast.compilers.diffusion_pipeline_compiler import compile, CompilationConfig
-
-def compile_model(model):
-    config = CompilationConfig.Default()
-
-    # xformers and Triton are suggested for achieving best performance.
-    # It might be slow for Triton to generate, compile and fine-tune kernels.
-    try:
-        import xformers
-
-        config.enable_xformers = True
-    except ImportError:
-        print("xformers not installed, skip")
-    # NOTE:
-    # When GPU VRAM is insufficient or the architecture is too old, Triton might be slow.
-    # Disable Triton if you encounter this problem.
-    try:
-        import triton
-
-        config.enable_triton = True
-    except ImportError:
-        print("Triton not installed, skip")
-
-    model = compile(model, config)
-    return model
-
 
 pipe = StableVideoDiffusionPipeline.from_pretrained(
   "stabilityai/stable-video-diffusion-img2vid-xt-1-1", torch_dtype=torch.float16, variant="fp16"
 ).to("cuda")
-pipe = compile_model(pipe)
 
 def get_raw_data(filename):
     # url_values = urllib.parse.urlencode(data)
