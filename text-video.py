e427769b7245b0a272a4079eda40fa679a42c2f3
Tony Kim <wukelong123@gmail.com>
2024-03-07 10:23:12 -0800

    kebu

--- text-video.py
+++ text-video.py
@@ -15,3 +15,11 @@ if len(args) > 3:
     filename = args[3]
 
 print('prompt text', prompt_text)
+
+pipeline_text2image = AutoPipelineForText2Image.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+).to("cuda")
+
+prompt = "Astronaut in a jungle, cold color palette, muted colors, detailed, 8k"
+image = pipeline_text2image(prompt=prompt_text, width=1024, height=576).images[0]
+image.save('image.jpg')
